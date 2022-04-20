4ae2988759b653e8abaa687ec43e7d26b91c948d
malfunctixn <malfunctixn@gmail.com>
2022-04-20 21:59:33 +0530

    Unable to read the dataset
    $ C:/Python310/python.exe "c:/Users/Malfunctixn/Desktop/Desktop extras/FYP/Code/main.py"
    Traceback (most recent call last):
      File "c:\Users\Malfunctixn\Desktop\Desktop extras\FYP\Code\main.py", line 6, in <module>
        data = pd.read_csv('data.csv')
      File "C:\Python310\lib\site-packages\pandas\util\_decorators.py", line 311, in wrapper
        return func(*args, **kwargs)
      File "C:\Python310\lib\site-packages\pandas\io\parsers\readers.py", line 680, in read_csv
        return _read(filepath_or_buffer, kwds)
      File "C:\Python310\lib\site-packages\pandas\io\parsers\readers.py", line 575, in _read
        parser = TextFileReader(filepath_or_buffer, **kwds)
      File "C:\Python310\lib\site-packages\pandas\io\parsers\readers.py", line 933, in __init__
        self._engine = self._make_engine(f, self.engine)
      File "C:\Python310\lib\site-packages\pandas\io\parsers\readers.py", line 1217, in _make_engine
        self.handles = get_handle(  # type: ignore[call-overload]
      File "C:\Python310\lib\site-packages\pandas\io\common.py", line 789, in get_handle
        handle = open(
    FileNotFoundError: [Errno 2] No such file or directory: 'data.csv'
    
    Note: The data set doesn't appear to be read by the program, we might have to shift to anaconda for the execution due to its vast libraries and easy running of the code.
    
    Signed-off-by: malfunctixn <malfunctixn@gmail.com>

--- Code/main.py
+++ Code/main.py
@@ -1,2 +1,9 @@
 # Breast Cancer Detection / Prediction System
 
+import pandas as pd
+import csv
+
+data = pd.read_csv('data.csv')
+data.describe()
+data.head()
+print("compilation complete\nno errors")
