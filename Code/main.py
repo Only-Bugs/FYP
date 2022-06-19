7cf465dccb23cf86f7efc96c9686b7706a73f64e
malfunctixn <malfunctixn@gmail.com>
2022-06-19 22:36:54 +0530

    Code: Clean up residual files and track kaggle notebook
    Signed-off-by: malfunctixn <malfunctixn@gmail.com>

--- Code/main.py
+++
@@ -1,9 +0,0 @@
-# Breast Cancer Detection / Prediction System
-
-import pandas as pd
-import csv
-
-data = pd.read_csv('data.csv')
-data.describe()
-data.head()
-print("compilation complete\nno errors")
